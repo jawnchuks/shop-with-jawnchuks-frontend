37d6de6d17b510e893cbaf418d01dc0e85bc427e
jawnchuks <jawnchuks@gmail.com>
2022-07-20 15:35:44 +0100

    fix:created accounts page template

---
+++ src/pages/account/index.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+import Layout from "@components/Layout/Layout";
+
+export default function UserAccount() {
+  return (
+    <div className="w-full bg-transparent overflow-hidden select-none  z-2">
+      <Layout
+        pageMeta={{
+          title: "",
+          description: "",
+        }}
+      ></Layout>
+    </div>
+  );
+}
